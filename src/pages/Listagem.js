b28c9dd5088c9c94fe30f5f39ab97ccf458980ff
Guilherme Pina <guilhermepinaramos@gmail.com>
2022-10-07 17:23:39 -0300

    Req 05

--- src/pages/Listagem.js
+++ src/pages/Listagem.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories } from '../services/api';
+import {
+  getCategories,
+  getProductsFromCategoryAndQuery,
+} from '../services/api';
 
 class Listagem extends React.Component {
   constructor() {
     super();
     this.state = {
       categories: [],
+      search: '',
+      productsSearch: [],
     };
   }
 
@@ -19,14 +24,43 @@ class Listagem extends React.Component {
     this.setState({ categories: arrayCategories });
   };
 
+  handleClick = async () => {
+    const { search } = this.state;
+    const response = await getProductsFromCategoryAndQuery('', search);
+    const { results } = response;
+    this.setState({
+      productsSearch: results,
+    });
+  };
+
+  handleSearch = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      search: value,
+    });
+  };
+
   render() {
-    const { categories } = this.state;
+    const { categories, productsSearch } = this.state;
     return (
       <div>
         <div>
           <label htmlFor="search">
-            <input type="text" id="search" name="search" />
+            <input
+              type="text"
+              id="search"
+              onChange={ this.handleSearch }
+              data-testid="query-input"
+              name="search"
+            />
           </label>
+          <button
+            type="button"
+            data-testid="query-button"
+            onClick={ this.handleClick }
+          >
+            Pesquisar
+          </button>
           <p data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>
@@ -39,17 +73,23 @@ class Listagem extends React.Component {
         </div>
         <div>
           <p>Categorias:</p>
-          {categories
-            .map((categoria) => (
-              <button
-                type="button"
-                key={ categoria.id }
-                data-testid="category"
-              >
-                { categoria.name }
-              </button>
-            ))}
+          {categories.map((categoria) => (
+            <button type="button" key={ categoria.id } data-testid="category">
+              {categoria.name}
+            </button>
+          ))}
         </div>
+
+        {
+          productsSearch.length > 0
+            ? productsSearch.map((item) => (
+              <div data-testid="product" key={ item.id }>
+                <p>{item.title}</p>
+                <img src={ item.thumbnail } alt={ item.name } />
+                <p>{item.price}</p>
+              </div>
+            )) : <p>Nenhum produto foi encontrado</p>
+        }
 
       </div>
     );
