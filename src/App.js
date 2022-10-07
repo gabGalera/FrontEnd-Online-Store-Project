7cd57ac1392d70a65faac4811a967b3aef8bd112
Gabriel Galera <gabgalera@hotmail.com>
2022-10-07 15:58:00 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Listagem from './pages/Listagem';
+import CarrinhoDeCompras from './pages/CarrinhoDeCompras';
 
 function App() {
   return (
     <BrowserRouter>
       <Listagem />
+      <Switch>
+        <Route path="/CarrinhoDeCompras" component={ CarrinhoDeCompras } />
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/pages/Listagem.js
+++ src/pages/Listagem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Listagem extends React.Component {
   render() {
@@ -10,6 +11,12 @@ class Listagem extends React.Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Link to="/CarrinhoDeCompras" data-testid="shopping-cart-button">
+          <button type="button">
+            Carrinho de Compras
+            <img src="./wireframes/card_03/png" alt="" />
+          </button>
+        </Link>
       </div>
     );
   }
