2f50669e57dea00d2e578b298e16295739fcb036
Gabriel Galera <gabgalera@hotmail.com>
2023-02-23 17:26:22 -0300

    Sincroniza a page CarrinhoDeCompras com o resto da aplicação

--- src/pages/CarrinhoDeCompras.jsx
+++ src/pages/CarrinhoDeCompras.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { loadShoppingCart, saveShoppingCart } from '../helpers/helpers';
 
 class CarrinhoDeCompras extends React.Component {
   constructor() {
@@ -10,7 +11,7 @@ class CarrinhoDeCompras extends React.Component {
   }
 
   componentDidMount() {
-    const result = this.loadShoppingCart();
+    const result = loadShoppingCart();
     this.setState({
       shoppingCart: result,
     });
@@ -28,6 +29,7 @@ class CarrinhoDeCompras extends React.Component {
       }
       return product;
     });
+    saveShoppingCart([...shoppingCart]);
     this.setState({
       shoppingCart,
     });
@@ -35,7 +37,7 @@ class CarrinhoDeCompras extends React.Component {
 
   removeItem = ({ target }) => {
     const { shoppingCart } = this.state;
-    shoppingCart.map((product) => {
+    shoppingCart.forEach((product) => {
       if (product.id === target.id) {
         if (product.quantity === 1) {
           return product;
@@ -45,13 +47,11 @@ class CarrinhoDeCompras extends React.Component {
       }
       return product;
     });
-
+    saveShoppingCart([...shoppingCart]);
     this.setState({
       shoppingCart,
     });
   };
-
-  loadShoppingCart = () => JSON.parse(localStorage.getItem('produtos'));
 
   render() {
     const { shoppingCart } = this.state;
@@ -59,59 +59,54 @@ class CarrinhoDeCompras extends React.Component {
       <div>
         {
           shoppingCart
-            ? shoppingCart.map((produto, index) => {
-              if (produto.quantity === undefined) {
-                produto.quantity = 1;
-              }
-              return (
-                <div key={ index }>
-                  <p data-testid="shopping-cart-product-name">
-                    { produto.title }
+            ? shoppingCart.map((produto, index) => (
+              <div key={ index }>
+                <p data-testid="shopping-cart-product-name">
+                  { produto.title }
+                </p>
+                <p>
+                  { produto.price }
+                </p>
+                <div>
+                  <button
+                    type="button"
+                    id={ produto.id }
+                    onClick={ this.addItem }
+                    data-testid="product-increase-quantity"
+                  >
+                    Somar
+                  </button>
+                  <p data-testid="shopping-cart-product-quantity">
+                    {
+                      produto.quantity
+                    }
                   </p>
-                  <p>
-                    { produto.price }
-                  </p>
-                  <div>
-                    <button
-                      type="button"
-                      id={ produto.id }
-                      onClick={ this.addItem }
-                      data-testid="product-increase-quantity"
-                    >
-                      Somar
-                    </button>
-                    <p data-testid="shopping-cart-product-quantity">
-                      {
-                        produto.quantity
-                      }
-                    </p>
-                    <button
-                      type="button"
-                      id={ produto.id }
-                      onClick={ this.removeItem }
-                      data-testid="product-decrease-quantity"
-                    >
-                      Diminuir
-                    </button>
-                    <button
-                      type="button"
-                      id={ produto.id }
-                      onClick={ (e) => {
-                        // const { shoppingCart } = this.state;
-                        const aux = shoppingCart
-                          .filter((entry) => entry.id !== e.target.id);
-                        this.setState({
-                          shoppingCart: aux,
-                        });
-                      } }
-                      data-testid="remove-product"
-                    >
-                      Excluir
-                    </button>
-                  </div>
+                  <button
+                    type="button"
+                    id={ produto.id }
+                    onClick={ this.removeItem }
+                    data-testid="product-decrease-quantity"
+                  >
+                    Diminuir
+                  </button>
+                  <button
+                    type="button"
+                    id={ produto.id }
+                    onClick={ (e) => {
+                      const aux = shoppingCart
+                        .filter((entry) => entry.id !== e.target.id);
+                      saveShoppingCart([...aux]);
+                      this.setState({
+                        shoppingCart: aux,
+                      });
+                    } }
+                    data-testid="remove-product"
+                  >
+                    Excluir
+                  </button>
                 </div>
-              );
-            })
+              </div>
+            ))
             : <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
         }
         <Link
